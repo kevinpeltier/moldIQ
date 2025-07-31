a8bcca5971519ea9002b909bc9ed7879d96d59ec
TheKevinPeltier <k-peltier@outlook.com>
2025-07-30 20:30:14 -0400

    boiler plate nav - restart

---
+++ components/nav.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul>
+        <img class="navLogo" id="navBarLogo" src="./images/LogoRev2.svg" alt="" >
+        <li><a href="home.php">HOME</a></li>
+        <li><a href="calc.php">NEW CALCULATION</a></li>
+        <li><a href="settings.php"><i class="fa fa-user-circle fa-2x" aria-hidden="true"></i></a></li>
+    </ul>
+</nav>
