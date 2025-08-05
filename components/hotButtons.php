a17c7be31693261a549ce508b135c7cf5faee8e5
TheKevinPeltier <k-peltier@outlook.com>
2025-08-05 17:14:38 -0400

    more interface tuning

--- components/hotButtons.php
+++ components/hotButtons.php
@@ -14,35 +14,66 @@
   <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-lg">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Create New Calculations Sheet</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
     
-        <form action="" class="row g3 needs-validation" novalidate>
-            <div class="col-md-6">
-                <label for="validationTextarea" class="form-label">Job Number</label>
-                <input type="number" class="form-control" id="railHeight validationTextarea" placeholder="Enter Job Number" required>
-                <div class="invalid-feedback">
-                    Please enter a message in the textarea.
+        <form action="" class="row g3 needs-validation p-2" novalidate>
+            <div class="d-flex flex-row">
+                <div class="col-md-6 p-3">
+                    <div class="">
+                        <label for="validationTextarea" class="form-label">Job Number</label>
+                        <input type="text" class="form-control" id="railHeight validationTextarea" placeholder="Enter Job Number" required>
+                        <div class="invalid-feedback">
+                            Please enter a message in the textarea.
+                        </div>
+                    </div>
+                    <div class="">
+                        <label for="validationTextarea" class="form-label">Rail Height</label>
+                        <input type="number" class="form-control" id="railHeight validationTextarea" step="0.0625" min="0" placeholder="Enter Rail Height" required>
+                        <div class="invalid-feedback">
+                            Please enter a message in the textarea.
+                        </div>
+                    </div>
+                    <div class="">
+                        <label for="validationTextarea" class="form-label">Ejector Plate Thickness</label>
+                        <input type="number" class="form-control" id="railHeight validationTextarea" step="0.0625" min="0" placeholder="Enter Ejector Plate Thickness" required>
+                        <div class="invalid-feedback">
+                            Please enter a message in the textarea.
+                        </div>
+                    </div>
+                    <div class="">
+                        <label for="validationTextarea" class="form-label">Retainer Plate Thickness</label>
+                        <input type="number" class="form-control" id="railHeight validationTextarea" step="0.0625" min="0" placeholder="Enter Retainer Plate Thickness" required>
+                        <div class="invalid-feedback">
+                            Please enter a message in the textarea.
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-6 d-flex align-items-center p-2">
+                    <img src="./images/EjectorBoxImg.png" alt="" class="w-100">
                 </div>
             </div>
-            <div class="col-md-6">
-                <label for="validationTextarea" class="form-label">Rail Height</label>
-                <input type="number" class="form-control" id="railHeight validationTextarea" placeholder="Enter Rail Height" required>
-                <div class="invalid-feedback">
-                    Please enter a message in the textarea.
-                </div>
-            </div>
-            <div class="col-md-6">
-                <label for="validationTextarea" class="form-label">Ejector Plate Thickness</label>
-                <input type="number" class="form-control" id="railHeight validationTextarea" placeholder="Enter Rail Height" required>
-                <div class="invalid-feedback">
-                    Please enter a message in the textarea.
-                </div>
+            <div class="col-md-12 d-flex flex-row">
+                    <div class="col-md-6 p-3">
+                        <label for="validationTextarea" class="form-label">Stop Button Thickness</label>
+                        <input type="number" class="form-control" id="railHeight validationTextarea" step="0.0625" min="0" placeholder="Enter Retainer Plate Thickness" required>
+                        <div class="invalid-feedback">
+                            Please enter a message in the textarea.
+                        </div>
+                    </div>
+                    <div class="col-md-6 p-3">
+                        <label for="validationTextarea" class="form-label">Travel Stop Thickness</label>
+                        <input type="number" class="form-control" id="railHeight validationTextarea" step="0.0625" min="0" placeholder="Enter Retainer Plate Thickness" required>
+                        <div class="invalid-feedback">
+                            Please enter a message in the textarea.
+                        </div>
+                    </div>
             </div>
             <div class="col-12 d-flex justify-content-center pt-5 pb-2">
-                <button type="submit" class="btn btn-primary w-100 ">Create New Calculations Sheet</button>
+                <button type="submit" class="btn btn-primary w-100 ">Create</button>
             </div>
 
 
--- components/nav.php
+++ components/nav.php
@@ -4,7 +4,10 @@
         <div class="py-2 px-3">
             <img src="./images/QikCal_Sm.svg" style="max-height: 35px;" alt="">
         </div>
-        <div class="px-3">
+        <div class="px-3 d-flex align-content-center gap-3">
+            <a href="#"><i class="fa fa-tachometer fa-2x" aria-hidden="true"></i></a>
+            <a href="#"><i class="fa fa-calculator fa-2x" aria-hidden="true"></i></a>
+            <div class="vr" style="height: 30px;"></div>
             <a href="#"><i class="fa fa-user-circle fa-2x" aria-hidden="true"></i></a>
         </div>
     </div>
