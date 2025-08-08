db5ef4b7be35d3f70a8937469c2d37ac16e6bfdd
TheKevinPeltier <k-peltier@outlook.com>
2025-08-07 21:06:18 -0400

    redesign. can't make my mind up

--- components/nav.php
+++ components/nav.php
@@ -1,15 +1,17 @@
-<nav class="pb-5 mb-5">
+<nav>
 
-    <div class="w-100 bg-light d-flex flex-row justify-content-between align-items-center fixed-top mb-3">
-        <div class="py-2 px-3">
-            <img src="./images/QikCal_Sm.svg" style="max-height: 35px;" alt="">
+    <img src="./images/QikCal_Sm.svg" style="max-height: 35px; max-width:50px;" alt="">
+
+
+    <div class="Nav-links">
+        <div class="grouped-Nav-links">
+            <a href="#">Dashboard</a>
+            <a href="#">Qik Calculation</i></a>
         </div>
-        <div class="px-3 d-flex align-content-center gap-3">
-            <a href="#"><i class="fa fa-tachometer fa-2x" aria-hidden="true"></i></a>
-            <a href="#"><i class="fa fa-calculator fa-2x" aria-hidden="true"></i></a>
-            <div class="vr" style="height: 30px;"></div>
-            <a href="#"><i class="fa fa-user-circle fa-2x" aria-hidden="true"></i></a>
-        </div>
+
+        <a href="#"><i class="fa fa-cog fa-2x" aria-hidden="true"></i>Settings</a>
+        <a href="#"><i class="fa fa-user-circle fa-2x" aria-hidden="true"></i></a>
     </div>
+
 
 </nav>
--- home.php
+++ home.php
@@ -12,32 +12,29 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <!-- Bootstap Link -->
-     <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.css">
-     <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.min.css">
+     <!-- <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.css">
+     <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.min.css"> -->
+
+
 
 </head>
 <body>
     
     <?php include './components/nav.php' ?>
 
-    <?php include './components/welcomeComp.php' ?>
-
-    <?php include './components/hotButtons.php' ?>
-
-    <?php include './components/tableListings.php'?>
 
 
 
 
-    <?php include './components/footer.php' ?>
+    <!-- <?php include './components/footer.php' ?> -->
 
 
     <!-- Reg Scripts -->
     <script src="./scripts/homeScript.js"></script>
 
     <!-- bootstrap Scripts -->
-     <script src="./node_modules/bootstrap/dist/js/bootstrap.js"></script>
-     <script src="./node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+     <!-- <script src="./node_modules/bootstrap/dist/js/bootstrap.js"></script>
+     <script src="./node_modules/bootstrap/dist/js/bootstrap.min.js"></script> -->
 
 </body>
 </html>
