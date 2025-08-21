f0a35763cad92d91f80a9a8d03f2ae1eba467389
TheKevinPeltier <k-peltier@outlook.com>
2025-08-21 18:20:22 -0400

    save point

--- components/homeInfoTable.php
+++ components/homeInfoTable.php
@@ -72,6 +72,205 @@
             </button>
         </div>
     </div>
+    <!-- test item #2 -->
+    <div class="main-row-item">
+        <div class="table-item-text">4567</div>
+        <div class="table-item-text">C Pillar</div>
+        <div class="table-item-text">Whatever</div>
+        <div class="table-item-text">Approved</div>
+        <div class="table-item-action-buttons">
+            <button class="std-button">
+                <i class="fa fa-pencil" aria-hidden="true"></i>
+            </button>
+            <button class="std-button">
+                <i class="fa fa-trash" aria-hidden="true"></i>
+            </button>
+        </div>
+    </div>
 
+    <!-- test item #3 -->
+    <div class="main-row-item">
+        <div class="table-item-text">6767</div>
+        <div class="table-item-text">Door Panel</div>
+        <div class="table-item-text">Down The Street</div>
+        <div class="table-item-text">Pending</div>
+        <div class="table-item-action-buttons">
+            <button class="std-button">
+                <i class="fa fa-pencil" aria-hidden="true"></i>
+            </button>
+            <button class="std-button">
+                <i class="fa fa-trash" aria-hidden="true"></i>
+            </button>
+        </div>
+    </div>
+    <!-- test item #2 -->
+    <div class="main-row-item">
+        <div class="table-item-text">4567</div>
+        <div class="table-item-text">C Pillar</div>
+        <div class="table-item-text">Whatever</div>
+        <div class="table-item-text">Approved</div>
+        <div class="table-item-action-buttons">
+            <button class="std-button">
+                <i class="fa fa-pencil" aria-hidden="true"></i>
+            </button>
+            <button class="std-button">
+                <i class="fa fa-trash" aria-hidden="true"></i>
+            </button>
+        </div>
+    </div>
 
+    <!-- test item #3 -->
+    <div class="main-row-item">
+        <div class="table-item-text">6767</div>
+        <div class="table-item-text">Door Panel</div>
+        <div class="table-item-text">Down The Street</div>
+        <div class="table-item-text">Pending</div>
+        <div class="table-item-action-buttons">
+            <button class="std-button">
+                <i class="fa fa-pencil" aria-hidden="true"></i>
+            </button>
+            <button class="std-button">
+                <i class="fa fa-trash" aria-hidden="true"></i>
+            </button>
+        </div>
+    </div>
+    <!-- test item #2 -->
+    <div class="main-row-item">
+        <div class="table-item-text">4567</div>
+        <div class="table-item-text">C Pillar</div>
+        <div class="table-item-text">Whatever</div>
+        <div class="table-item-text">Approved</div>
+        <div class="table-item-action-buttons">
+            <button class="std-button">
+                <i class="fa fa-pencil" aria-hidden="true"></i>
+            </button>
+            <button class="std-button">
+                <i class="fa fa-trash" aria-hidden="true"></i>
+            </button>
+        </div>
+    </div>
+
+    <!-- test item #3 -->
+    <div class="main-row-item">
+        <div class="table-item-text">6767</div>
+        <div class="table-item-text">Door Panel</div>
+        <div class="table-item-text">Down The Street</div>
+        <div class="table-item-text">Pending</div>
+        <div class="table-item-action-buttons">
+            <button class="std-button">
+                <i class="fa fa-pencil" aria-hidden="true"></i>
+            </button>
+            <button class="std-button">
+                <i class="fa fa-trash" aria-hidden="true"></i>
+            </button>
+        </div>
+    </div>
+    <!-- test item #2 -->
+    <div class="main-row-item">
+        <div class="table-item-text">4567</div>
+        <div class="table-item-text">C Pillar</div>
+        <div class="table-item-text">Whatever</div>
+        <div class="table-item-text">Approved</div>
+        <div class="table-item-action-buttons">
+            <button class="std-button">
+                <i class="fa fa-pencil" aria-hidden="true"></i>
+            </button>
+            <button class="std-button">
+                <i class="fa fa-trash" aria-hidden="true"></i>
+            </button>
+        </div>
+    </div>
+
+    <!-- test item #3 -->
+    <div class="main-row-item">
+        <div class="table-item-text">6767</div>
+        <div class="table-item-text">Door Panel</div>
+        <div class="table-item-text">Down The Street</div>
+        <div class="table-item-text">Pending</div>
+        <div class="table-item-action-buttons">
+            <button class="std-button">
+                <i class="fa fa-pencil" aria-hidden="true"></i>
+            </button>
+            <button class="std-button">
+                <i class="fa fa-trash" aria-hidden="true"></i>
+            </button>
+        </div>
+    </div>
+    <!-- test item #3 -->
+    <div class="main-row-item">
+        <div class="table-item-text">6767</div>
+        <div class="table-item-text">Door Panel</div>
+        <div class="table-item-text">Down The Street</div>
+        <div class="table-item-text">Pending</div>
+        <div class="table-item-action-buttons">
+            <button class="std-button">
+                <i class="fa fa-pencil" aria-hidden="true"></i>
+            </button>
+            <button class="std-button">
+                <i class="fa fa-trash" aria-hidden="true"></i>
+            </button>
+        </div>
+    </div>
+    
+    
+    <!-- test item #3 -->
+    <div class="main-row-item">
+        <div class="table-item-text">6767</div>
+        <div class="table-item-text">Door Panel</div>
+        <div class="table-item-text">Down The Street</div>
+        <div class="table-item-text">Pending</div>
+        <div class="table-item-action-buttons">
+            <button class="std-button">
+                <i class="fa fa-pencil" aria-hidden="true"></i>
+            </button>
+            <button class="std-button">
+                <i class="fa fa-trash" aria-hidden="true"></i>
+            </button>
+        </div>
+    </div>
+    <!-- test item #3 -->
+    <div class="main-row-item">
+        <div class="table-item-text">6767</div>
+        <div class="table-item-text">Door Panel</div>
+        <div class="table-item-text">Down The Street</div>
+        <div class="table-item-text">Pending</div>
+        <div class="table-item-action-buttons">
+            <button class="std-button">
+                <i class="fa fa-pencil" aria-hidden="true"></i>
+            </button>
+            <button class="std-button">
+                <i class="fa fa-trash" aria-hidden="true"></i>
+            </button>
+        </div>
+    </div>
+    <!-- test item #3 -->
+    <div class="main-row-item">
+        <div class="table-item-text">6767</div>
+        <div class="table-item-text">Door Panel</div>
+        <div class="table-item-text">Down The Street</div>
+        <div class="table-item-text">Pending</div>
+        <div class="table-item-action-buttons">
+            <button class="std-button">
+                <i class="fa fa-pencil" aria-hidden="true"></i>
+            </button>
+            <button class="std-button">
+                <i class="fa fa-trash" aria-hidden="true"></i>
+            </button>
+        </div>
+    </div>
+    <!-- test item #3 -->
+    <div class="main-row-item">
+        <div class="table-item-text">6767</div>
+        <div class="table-item-text">Door Panel</div>
+        <div class="table-item-text">Down The Street</div>
+        <div class="table-item-text">Pending</div>
+        <div class="table-item-action-buttons">
+            <button class="std-button">
+                <i class="fa fa-pencil" aria-hidden="true"></i>
+            </button>
+            <button class="std-button">
+                <i class="fa fa-trash" aria-hidden="true"></i>
+            </button>
+        </div>
+    </div>
 </div>
